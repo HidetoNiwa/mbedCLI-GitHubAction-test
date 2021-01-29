ad42e25fa702dc06b7406cbfce6e22eb962164ec
HidetoNiwa <tototohide@gmail.com>
2021-01-29 21:36:21 +0900

    first commit

---
+++ .github/mbedCLI/entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -eux
+
+mbed compile -t GCC_ARM -m NUCLEO_F446RE
+
+today=`date "+%Y%m%d%H%M%S"`
+# create release
+res=`curl -H "Authorization: token $GITHUB_TOKEN" -X POST https://api.github.com/repos/$GITHUB_REPOSITORY/releases \
+-d "
+{
+  \"tag_name\": \"$today\",
+  \"target_commitish\": \"$GITHUB_SHA\",
+  \"name\": \"$today\",
+  \"draft\": false,
+  \"prerelease\": false
+}"`
+
+# extract release id
+rel_id=`echo ${res} | python3 -c 'import json,sys;print(json.load(sys.stdin)["id"])'`
+
+# upload built pdf
+curl -H "Authorization: token $GITHUB_TOKEN" -X POST https://uploads.github.com/repos/$GITHUB_REPOSITORY/releases/${rel_id}/assets?name=main.pdf\
+--header 'Content-Type: application/pdf'\
+--upload-file main.pdf
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <mbed.h>
+
+DigitalOut led(LED1);
+
+int main(){
+    while(1){
+        led=!led;
+    }
+}
