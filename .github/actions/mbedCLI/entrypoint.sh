98c62b47fe99b3bea94be9de2ccd940056c5e485
HidetoNiwa <tototohide@gmail.com>
2021-01-30 02:04:25 +0900

    set ubuntu version

--- .github/actions/mbedCLI/entrypoint.sh
+++ .github/actions/mbedCLI/entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -eux
 
+apt update
+
+apt upgrade -Y
+
+apt install gcc-arm-none-eabi -y
+
 pip3 install mbed-cli
 
 pip3 install pyyaml jsonschema mbed_cloud_sdk jinja2 mbed_ls mbed_host_tests mbed_greentea pyelftools manifest_tool icetea pycryptodome pyusb cmsis_pack_manager psutil cryptography click cbor
